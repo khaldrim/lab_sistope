2351de3bfc02c7179c135640e15be0c3a415b204
Héctor Pérez M <hector.perez.m@usach.cl>
2018-03-29 18:58:39 -0300

    funcion getopt, sin validaciones

---
+++ lab_1/funciones.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+    Entrada    : Ingresan los valores de las banderas solicitadas como parametros al usuario.
+    Salida     : Retorna un entero, con dos valores posibles 1 y 0. Retorna 1 si la funcion cumplio su flujo normal y retorna 0 si existe algun error.
+    Descripcion: Funcion que bla bla... 
+*/
+int mainMenu(int cflag, int uflag, int nflag, int bflag)
+{
+    //Llamar a la funcion que lee las imagenes
+
+    //Llamar a la funcion que convierte a escala de grises
+
+    //Llamar a la funcion que binariza la imagen
+
+    //llamar a la funcion que clasifica
+    return 1;
+}
